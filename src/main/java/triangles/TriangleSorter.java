5d8704c9b47aef7ce35838f8bcefae3c0936ab0a
Benjamin Williams <deanpbr@gmail.com>
2019-06-04 23:00:34 -0400

    Refactoring conditionals into a method.

--- src/main/java/triangles/TriangleSorter.java
+++ src/main/java/triangles/TriangleSorter.java
@@ -2,10 +2,14 @@ package triangles;
 
 public class TriangleSorter {
     public String analyze(int sideA, int sideB, int sideC) {
-        if (sideA == sideB && sideB == sideC) {
+        if (isAnEquilateral(sideA, sideB, sideC)) {
             return "Equilateral";
         } else {
             return "Isosceles";
         }
+    }
+
+    private boolean isAnEquilateral(int sideA, int sideB, int sideC) {
+        return sideA == sideB && sideB == sideC;
     }
 }
