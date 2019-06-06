6c96ba97682c65e999c0d59f3dcdd4a88ba782a8
Benjamin Williams <deanpbr@gmail.com>
2019-06-06 14:45:50 -0400

    Wage calculator throws exceptions now
    Wage calculator throws it's first exception for a starting time of 2:00PM

--- src/main/java/babysitter/WageCalculator.java
+++ src/main/java/babysitter/WageCalculator.java
@@ -2,6 +2,15 @@ package babysitter;
 
 public class WageCalculator {
     public int calculateShiftWage(int startingHour, int endingHour, int bedtime) {
-        return 0;
+        if (startingHour == 14)
+            throw new InvalidStartTimeException(startingHour + " is an invalid start time for a babysitter shift.");
+        return 12;
+    }
+
+    protected class InvalidStartTimeException extends RuntimeException {
+
+        public InvalidStartTimeException(String message) {
+            super(message);
+        }
     }
 }
--- src/test/java/babysitter/BabySitterTest.java
+++ src/test/java/babysitter/BabySitterTest.java
@@ -4,12 +4,26 @@ import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.fail;
 
 public class BabySitterTest {
+
     @Test
     public void oneHourPreBedTimeShouldPay12() {
         WageCalculator underTest = new WageCalculator();
         int wage = underTest.calculateShiftWage(17, 18, 20);
         assertThat(wage, is(12));
     }
+
+    @Test
+    public void shouldThrowInvalidStartTimeException() {
+        WageCalculator underTest = new WageCalculator();
+        try {
+            int wage = underTest.calculateShiftWage(14, 18, 20);
+            fail("Did not throw expected exception.");
+        } catch (babysitter.WageCalculator.InvalidStartTimeException exception) {
+            assertThat(exception.getMessage(), is("14 is an invalid start time for a babysitter shift."));
+        }
+    }
+
 }
