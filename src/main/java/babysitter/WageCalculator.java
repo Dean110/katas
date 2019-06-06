294708233db5b9c1078f9deadb996f4dff6b4967
Benjamin Williams <deanpbr@gmail.com>
2019-06-06 16:36:17 -0400

    Refactored exception message
    After some reflection, I moved the exception message to the exception class itself.  The WageCalculator shouldn't know what the exception has to say, just that an exception needs to be thrown.

--- src/main/java/babysitter/WageCalculator.java
+++ src/main/java/babysitter/WageCalculator.java
@@ -3,14 +3,15 @@ package babysitter;
 public class WageCalculator {
     public int calculateShiftWage(int startingHour, int endingHour, int bedtime) {
         if (startingHour == 14)
-            throw new InvalidStartTimeException(startingHour + " is an invalid start time for a babysitter shift.");
+            throw new InvalidStartTimeException(startingHour);
         return 12;
     }
 
     protected class InvalidStartTimeException extends RuntimeException {
 
-        public InvalidStartTimeException(String message) {
-            super(message);
+        public InvalidStartTimeException(int startTime) {
+
+            super(startTime + " is an invalid start time for a babysitter shift.");
         }
     }
 }
