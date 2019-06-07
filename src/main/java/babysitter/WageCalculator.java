e99643410e91f8d2e57ddcd4e70d471360e927e6
Benjamin Williams <deanpbr@gmail.com>
2019-06-06 22:43:12 -0400

    Refactoring
    Extracted a method to convert the before noon hours to values that are able to be used when making shift calculations.

--- src/main/java/babysitter/WageCalculator.java
+++ src/main/java/babysitter/WageCalculator.java
@@ -11,19 +11,24 @@ public class WageCalculator {
         return 12;
     }
 
-    private boolean shiftIsInvalid(int startingHour, int endingHour) {
-        if (startingHour<12){
-            startingHour +=24;
-        }
-        return startingHour>=endingHour;
-    }
-
     private boolean startingTimeIsInvalid(int startingHour) {
         return startingHour < 17 && startingHour > 3;
     }
 
     private boolean endingTimeIsInvalid(int endingHour) {
         return endingHour > 4 && endingHour < 18;
+    }
+
+    private boolean shiftIsInvalid(int startingHour, int endingHour) {
+        startingHour = convertTimeToShiftRelativeTime(startingHour);
+        return startingHour>=endingHour;
+    }
+
+    private int convertTimeToShiftRelativeTime(int startingHour) {
+        if (startingHour<12){
+            startingHour +=24;
+        }
+        return startingHour;
     }
 
 }
