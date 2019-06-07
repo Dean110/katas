0341158dcb93777e1d6f86adef601b06d5380e51
Benjamin Williams <deanpbr@gmail.com>
2019-06-06 23:13:02 -0400

    Refactoring shift time validations into their own method.

--- src/main/java/babysitter/WageCalculator.java
+++ src/main/java/babysitter/WageCalculator.java
@@ -2,17 +2,22 @@ package babysitter;
 
 public class WageCalculator {
     public int calculateShiftWage(int startingHour, int endingHour, int bedtime) {
+        validateShiftTimes(startingHour, endingHour);
+
+        if(startingHour>=bedtime)
+            return 8;
+        if(startingHour>=0&& startingHour<4)
+            return 16;
+        return 12;
+    }
+
+    private void validateShiftTimes(int startingHour, int endingHour) {
         if (startingTimeIsInvalid(startingHour))
             throw new InvalidStartTimeException(startingHour);
         if (endingTimeIsInvalid(endingHour))
             throw new InvalidEndTimeException(endingHour);
         if(shiftIsInvalid(startingHour, endingHour))
             throw new InvalidTimePunchesException();
-        if(startingHour>=bedtime)
-            return 8;
-        if(startingHour>=0&& startingHour<4)
-            return 16;
-        return 12;
     }
 
     private boolean startingTimeIsInvalid(int startingHour) {
